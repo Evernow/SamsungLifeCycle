0a1925441124007cb6bbb70af6a2a45f2171ab12
Evernow <44484725+Evernow@users.noreply.github.com>
2023-01-22 09:08:43 -0500

    Initial commit

---
+++ .github/workflows/samsung.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+from itertools import chain
+from bs2json import bs2json
+r = requests.get("https://security.samsungmobile.com/workScope.smsb")
+soup=BeautifulSoup(r.text,'html.parser')
+tags = soup.find_all(class_="txt_section")
+converter = bs2json()
+Device_Support = {}
+
+for securitybranch in converter.convertAll(tags,join=True)[0]['div']:
+    try:
+        devices = []
+        for device in securitybranch['ul']['li']:
+            devices.append(device['text'].split(','))
+        # print(securitybranch['strong']['text'])
+        # print(devices)
+        if 'Disclaimer' not in securitybranch['strong']['text']:
+            Device_Support[securitybranch['strong']['text']] = list(chain.from_iterable(devices))
+    except:
+        pass
+import os
+if os.path.exists("SamsungReleases.json"):
+  os.remove("SamsungReleases.json")
+with open('SamsungReleases.json', 'w') as fp:
+    json.dump(Device_Support, fp, indent = 2)
+
+print(Device_Support)
